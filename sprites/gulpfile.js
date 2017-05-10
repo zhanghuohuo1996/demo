deb320306624bc863df3f78a73a18735ab30ffe3
zhanghuohuo <zhanghuohuo1996@sina.com>
2017-05-10 13:06:05 +0800

    sprites gulp

---
+++ sprites/gulpfile.js
@@ -0,0 +1,13 @@
+﻿var gulp=require('gulp');
+var spritesmith=require('gulp.spritesmith');
+gulp.task('default',function(){
+	return gulp.src('images/*.png')
+	.pipe(spritesmith({
+		imgName:'sprite.png',
+		cssName:'css/sprite.css',
+		padding:5,
+		algorithm:'binary-tree',
+		cssTemplate:'css/handlestr.css'
+	}))
+	.pipe(gulp.dest('dist/'));
+});
